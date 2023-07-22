09ba09775bfa36171da6111a9e11c9ead4742526
Moaz <99875565+moaazelsharkawy@users.noreply.github.com>
2023-07-22 17:34:44 +0300

    Update pull to refresh.js

--- pull to refresh.js
+++ pull to refresh.js
@@ -1,18 +1,13 @@
 <script>
-// احضر عنصر الجسم (أو العنصر الذي ترغب في إعادة تحميله)
 var bodyElement = document.querySelector('body');
 
-// تعيين المتغيرات اللازمة لتنفيذ عملية السحب لأسفل
 var startY = 0;
-var threshold = 100; // حد السحب المطلوب لتنفيذ إعادة التحميل
+var threshold = 100; 
 
-// تعيين الدالة التي ستتم استدعاؤها عند الانتهاء من عملية السحب لأسفل
 function onPullToRefresh() {
-    // قم بتنفيذ إجراءات إعادة التحميل هنا
     location.reload();
 }
 
-// تعيين الأحداث لعملية السحب لأسفل
 bodyElement.addEventListener('touchstart', function(e) {
     startY = e.touches[0].clientY;
 });
@@ -23,11 +18,11 @@ bodyElement.addEventListener('touchmove', function(e) {
 
     if (distance > threshold && window.scrollY === 0) {
         var touchendY = e.changedTouches[0].clientY;
-        var refreshThreshold = 200; // حد السحب المطلوب لتنفيذ إعادة التحميل
+        var refreshThreshold = 200; 
 
         if (touchendY - startY > refreshThreshold) {
             onPullToRefresh();
-            startY = touchendY; // إعادة تعيين نقطة البداية لتمكين السحب المستمر
+            startY = touchendY; 
         }
     }
 });
